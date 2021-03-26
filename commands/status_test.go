3821c16d272b368c89a30f0cad473e96e43f99a2
SBanczyk <sbanczyk@interia.pl>
2021-03-26 08:45:35 +0100

    testing status

--- commands/status_test.go
+++ commands/status_test.go
@@ -24,10 +24,17 @@ func TestStatus(t *testing.T) {
 	backupPath := path.Join(configDir, "backupfiles")
 	assert.NoError(t, os.Mkdir(localDir, 0777))
 	assert.NoError(t, os.Mkdir(backupDir, 0777))
+	newLocalPath := path.Join(localDir, "newLocal")
+	assert.NoError(t, os.Mkdir(newLocalPath, 0777))
 	err = commands.InitFs(localDir, backupDir)
 	assert.NoError(t, err)
 	createLocalFile(t, localDir, "modifiedFile")
 	createLocalFile(t, localDir, "modifiedUnstage")
+	createLocalFile(t, localDir, "untrackedFile")
+	createLocalFile(t, newLocalPath, "modifiedFile1")
+	createLocalFile(t, newLocalPath, "modifiedUnstage1")
+	createLocalFile(t, newLocalPath, "untrackedFile1")
+	createLocalFile(t, newLocalPath, "newFile1")
 	err = model.SaveStaging(stagingPath, &model.Staging{
 		StagingFiles: []model.StagingPath{
 			{
@@ -42,10 +49,23 @@ func TestStatus(t *testing.T) {
 				Path:   "modifiedFile",
 				Shadow: true,
 			},
+			{
+				Path:   path.Join("newLocal", "missingFromStaging1"),
+				Shadow: true,
+			},
+			{
+				Path:   path.Join("newLocal", "newFile1"),
+				Shadow: true,
+			},
+			{
+				Path:   path.Join("newLocal", "modifiedFile1"),
+				Shadow: true,
+			},
 		},
-		DestroyedFiles: []string{"destroyedFile"},
+		DestroyedFiles: []string{"destroyedFile", path.Join("newLocal", "destroyedFile1")},
 	})
 	createLocalFile(t, localDir, "newFile")
+
 	assert.NoError(t, err)
 	err = model.SaveBackup(backupPath, &model.Backup{
 		Files: map[string][]model.BackupPath{
@@ -64,6 +84,22 @@ func TestStatus(t *testing.T) {
 				},
 				{
 					Path:   "modifiedUnstage",
+					Shadow: true,
+				},
+				{
+					Path:   path.Join("newLocal", "modifiedFile1"),
+					Shadow: true,
+				},
+				{
+					Path:   path.Join("newLocal", "destroyedFile1"),
+					Shadow: true,
+				},
+				{
+					Path:   path.Join("newLocal", "missingFromBack1"),
+					Shadow: false,
+				},
+				{
+					Path:   path.Join("newLocal", "modifiedUnstage1"),
 					Shadow: true,
 				},
 			},
